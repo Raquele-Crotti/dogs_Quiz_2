d95ec37b8e3eee8fb4c39a25764568f2f667d573
Raquele Crotti <raquelecrotti@gmail.com>
2019-11-11 23:23:58 +0000

    Added controller dogs, generated model dog, Implemented bootstrap and simpleForm gem, added a simple form to the application to enter dog's name and description and had the database save it and display user input on index page

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,6 +1,24 @@
 class DogsController < ApplicationController
   
   def index
+    @dog = Dog.order("RANDOM()").first
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    Dog.create(dog_params)
+    redirect_to root_dogs_path
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :description)
   end
 
 end
+
+
